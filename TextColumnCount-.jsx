178147391beba8f41ad2a37dbf7023eb08158465
vetl1489 <vetl1489@gmail.com>
2023-09-07 02:52:42 +0300

    fif if

--- TextColumnCount-.jsx
+++ TextColumnCount-.jsx
@@ -8,7 +8,7 @@ app.scriptPreferences.enableRedraw = false;
 for (var i = 0; app.selection.length > i;  i++) {
   if (app.selection[i].constructor.name === "TextFrame") {
     var count = app.selection[i].textFramePreferences.textColumnCount;
-    if (count < MAX_COLUMN && count > 1) {
+    if (count > 1) {
       app.selection[i].textFramePreferences.textColumnCount--;
     }
   }
